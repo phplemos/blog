65c37ce8298d159c05776a18263adce1567fa842
Pedro Henrique Pinheiro Lemos <85008833+phplemos@users.noreply.github.com>
2026-03-12 17:23:06 -0300

    Update page_url in notion-hugo.config.ts

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -4,7 +4,7 @@ const userConfig: UserConfig = {
     base_url: "https://notion-hugo.pages.dev",
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/phplemos-dev/PHPLEMOS-Blog-6c7b8e18e88b8226948b81a8a160da54',
         pages: [
             // {
             //     page_id: '<page_id>',
